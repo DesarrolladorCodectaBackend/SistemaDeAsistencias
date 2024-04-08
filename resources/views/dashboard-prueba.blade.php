ea78bc516a092b8cc65504de5d3be51b01ea0df8
Cristopher De la Cruz <cristopher.delacruz.1555@email.com>
2024-04-08 11:20:55 -0500

    Side-nav-bar | DashboardPrueba

---
+++ resources/views/dashboard-prueba.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>DASHBOARD PRUEBA</title>
+</head>
+
+<body>
+    <div id="wrapper">
+        @include('components.inspinia.side_nav_bar-inspinia')
+        
+    </div>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,10 +22,22 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/dashboard-prueba', function () {
+    return view('dashboard-prueba');
+});
+
+Route::get('/sidebar-inspinia', function () {
+    return view('components.inspinia.sidebar-inspinia');
+});
+
+Route::get('/navbar-inspinia', function () {
+    return view('components.inspinia.navbar-inspinia');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
