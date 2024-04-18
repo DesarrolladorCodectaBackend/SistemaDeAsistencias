0bef06eda5b61d437ac22c82fa43898536249b5d
Zicoespinozac <junior2001perezg@gmail.com>
2024-04-18 16:05:10 -0500

    modificacion de colaboradores ,computadora colaborador, horarios virtuales colaborador, programas instalados

--- app/Http/Controllers/Programas_instaladosController.php
+++ app/Http/Controllers/Programas_instaladosController.php
@@ -12,33 +12,9 @@ class Programas_instaladosController extends Controller
 
     public function index()
     {
-        try {
-            $programas_instalados = Programas_instalados::with([
-                'computadora_colaborador' => function ($query) {
-                    $query->select(
-                        'id',
-                        'colaborador_id',
-                        'procesador',
-                        'tarjeta_grafica',
-                        'ram',
-                        'almacenamiento',
-                        'es_laptop',
-                        'codigo_serie'
-                    );
-                },
-                'programas' => function ($query) {
-                    $query->select('id', 'nombre', 'descripcion', 'memoria_grafica', 'ram'); }
-            ])
-                ->get();
+        $programas_instalados = Programas_instalados::all();
 
-            if (count($programas_instalados) == 0) {
-                return response()->json(["resp" => "No hay registros insertados"]);
-            }
-
-            return response()->json(["data" => $programas_instalados, "conteo" => count($programas_instalados)]);
-        } catch (Exception $e) {
-            return response()->json(["Error" => $e]);
-        }
+        return view('programas_instalados.index', compact('programas_instalados'));
 
     }
 
@@ -76,6 +52,23 @@ class Programas_instaladosController extends Controller
 
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'computadora_id' => 'required|integer|min:1|max:100',
+            'programa_id' => 'required|integer|min:1|max:255'
+        ]);
+        
+            
+        Programas_instalados::create([
+            "computadora_id" => $request->computadora_id,
+            "programa_id" => $request->programa_id
+        ]);
+
+        return redirect()->route('programas_instalados.index');
+    }
+
+
 
     public function show($programas_instalados_id)
     {
@@ -111,61 +104,26 @@ class Programas_instaladosController extends Controller
 
     public function update(Request $request, $programas_instalados_id)
     {
-        DB::beginTransaction();
-        try{
-            $programas_instalados = Programas_instalados::find($programas_instalados_id);
+        $request->validate([
+            'computadora_id' => 'required|integer|min:1|max:100',
+            'programa_id' => 'required|integer|min:1|max:255',
+        ]);
 
-            if(!$programas_instalados){
-                return response()->json(["resp" => "No existe un registro con ese id"]);
-            }
-
-            if(!$request->computadora_id){
-                return response()->json(["resp"=> "ingrese computadora"]);
-            }
-
-            if(!$request->programa_id){
-                return response()->json(["resp" => "ingrese programa"]);
-            }
-
-            if (!is_integer($request->computadora_id)){
-                return response()->json(["resp"=> "El id de la computadora debe ser un número entero"]);
-            }
-
-            if (!is_integer($request->programa_id)){
-                return response()->json(["resp"=> "El id del programa debe ser un número entero"]);
-            }
-
-            $programas_instalados->fill([
-                "computadora_id" => $request->computadora_id,
-                "programa_id" => $request->programa_id
-            ])->save();
+        $programas_instalados = Programas_instalados::findOrFail($programas_instalados_id);
         
-            DB::commit();
-            return response()->json(["resp" => "Registro actualizado correctamente"]);
-        } catch(Exception $e){
-            DB::rollBack();
-            return response()->json(["error"=> $e]);
-        }
+        $programas_instalados->update($request->all());
+
+        return redirect()->route('programas_instalados.index');
 
     }
 
 
     public function destroy($programas_instalados_id)
     {
-        DB::beginTransaction();
-        try{
-            $programas_instalados = Programas_instalados::find($programas_instalados_id);
+        $programas_instalados = Programas_instalados::findOrFail($programas_instalados_id);
 
-            if(!$programas_instalados){
-                return response()->json(["resp" => "No existe un registro con ese id"]);
-            }
+        $programas_instalados->delete();
 
-            $programas_instalados->delete();
-            DB::commit();
-            return response()->json(["resp" => "Registro eliminado correctamente"]);
-        } catch (Exception $e){
-            DB::rollBack();
-            return response()->json(["error"=> $e]);
-        }
+        return redirect()->route('programas_instalados.index');
     }
 }
