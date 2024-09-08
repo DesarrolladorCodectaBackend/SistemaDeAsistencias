37650444a4ce69950f4729abdc90103f590165f9
PedroBM <pedrobecmucha12@gmail.com>
2024-09-07 20:09:50 -0500

    a

--- app/Http/Controllers/InformesSemanalesController.php
+++ app/Http/Controllers/InformesSemanalesController.php
@@ -17,6 +17,7 @@ class InformesSemanalesController extends Controller
 
     public function store(Request $request)
     {
+        // obtiene el a;o, mes, area_id, semana_id para la creacion de dicho informe ubicandolo en las semanas, anios, mes y area respectivas
         $year = $request->year;
         $mes = $request->mes;
         $area_id = $request->area_id;
@@ -42,10 +43,59 @@ class InformesSemanalesController extends Controller
         return redirect()->route('responsabilidades.asis', ['year' => $year, 'mes' => $mes,'area_id' => $area_id]);
     }
 
-    public function update()
+    public function update(Request $request, $InformeSemanal)
     {
-        //
+        $year = $request->year;
+        $mes = $request->mes;
+        $area_id = $request->area_id;
+        $semana_id = $request->semana_id;
+
+        $informe = InformeSemanal::findOrFail($InformeSemanal);
+
+        // Actualizar el archivo si se sube uno nuevo
+        if ($request->hasFile('informe_url')) {
+            // Elimina el archivo existente
+            if ($informe->informe_url && file_exists(public_path('storage/informes/' . $informe->informe_url))) {
+                unlink(public_path('storage/informes/' . $informe->informe_url));
+            }
+
+            // Subir el nuevo archivo
+            $archivo = $request->file('informe_url');
+            $nombreInforme = time() . '.' . $archivo->getClientOriginalExtension();
+            $archivo->move(public_path('storage/informes'), $nombreInforme);
+
+            // Actualizar la ruta del archivo en la base de datos
+            $informe->informe_url = $nombreInforme;
+        }
+
+        $informe->update([
+            'titulo' => $request->titulo,
+            'nota_semanal' => $request->nota_semanal,
+            'semana_id' => $request->semana_id,
+            'area_id' => $request->area_id,
+        ]);
+
+        return redirect()->route('responsabilidades.asis', ['year' => $year, 'mes' => $mes,'area_id' => $area_id]);
     }
+
+
+    public function show($InformeSemanal)
+{
+    // // Obtener el informe con el ID proporcionado
+    // $informe = InformeSemanal::findOrFail($InformeSemanal);
+
+    // // Obtener los valores necesarios para la vista
+    // $year = $informe->year; // o de alguna manera calculado/recuperado
+    // $mes = $informe->mes; // o de alguna manera calculado/recuperado
+    // $semana_id = $informe->semana_id;
+    // $area_id = $informe->area_id;
+
+    // // Pasar todos los datos a la vista
+    // return view('inspiniaViews.responsabilidades.asistencia', compact('informe', 'year', 'mes', 'semana_id', 'area_id'));
+}
+
+
+
 
     public function delete()
     {
