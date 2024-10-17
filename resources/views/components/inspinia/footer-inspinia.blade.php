b73e40bf78545849b1a2274d679e18930f048768
Cristopher-De-La-Cruz <cristopher.delacruz.1555@gmail.com>
2024-10-17 17:09:32 -0500

    footer

--- resources/views/components/inspinia/footer-inspinia.blade.php
+++ resources/views/components/inspinia/footer-inspinia.blade.php
@@ -1,8 +1,8 @@
-<div class="footer">
-    <div class="float-right">
-        10GB of <strong>250GB</strong> Free.
-    </div>
+<div class="d-flex justify-content-between py-2 px-2 mt-2 cursor-pointer">
     <div>
         <strong>Copyright</strong> Example Company &copy; 2014-2018
+    </div>
+    <div class="">
+        10GB of <strong>250GB</strong> Free.
     </div>
 </div>
