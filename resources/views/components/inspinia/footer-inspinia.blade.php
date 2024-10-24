3db61e6cc92f3bdb41a7fc4a8950c8105a09a6fb
Coolmesito <msamaniegocalderon@gmail.com>
2024-10-24 15:17:36 -0500

    Mailing Count

--- resources/views/components/inspinia/footer-inspinia.blade.php
+++ resources/views/components/inspinia/footer-inspinia.blade.php
@@ -1,8 +1,16 @@
-<div class="d-flex justify-content-between py-2 px-2 mt-2">
+{{-- <div class="d-flex justify-content-between py-2 px-2 mt-2">
     <div>
         <strong>Copyright</strong> Example Company &copy; 2014-2018
     </div>
     <div>
         10GB of <strong>250GB</strong> Free.
+    </div>
+</div> --}}
+<div class="footer">
+    <div class="float-right">
+        10GB of <strong>250GB</strong> Free.
+    </div>
+    <div>
+        <strong>Copyright</strong> Example Company &copy; 2014-2018
     </div>
 </div>
