61d245f398fdfe2338a2421ecf7f519e916b1679
pedrobm2912 <pedrobecmucha12@gmail.com>
2025-09-17 11:54:54 -0500

    cambios chicos

--- app/Http/Controllers/Computadora_colaboradorController.php
+++ app/Http/Controllers/Computadora_colaboradorController.php
@@ -48,7 +48,7 @@ class Computadora_colaboradorController extends Controller
         DB::beginTransaction();
         try{
             $request->validate([
-                'colaborador_id' => 'required|integer|min:1|max:100',
+                'colaborador_id' => 'required|integer',
                 'es_laptop' =>  'required|boolean',
                 'codigo_serie' =>  'required|string',
                 'procesador' => 'required|string',
