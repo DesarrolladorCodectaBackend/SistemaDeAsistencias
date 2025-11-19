ac7fa473222e0050775196875738f0f451f8dbb3
anonimo <cambialo@gmail.com>
2025-11-19 11:00:27 -0500

    se agrego redes sociales al footer

--- resources/views/components/inspinia/footer-inspinia.blade.php
+++ resources/views/components/inspinia/footer-inspinia.blade.php
@@ -1,16 +1,38 @@
-<!-- <div class="d-flex justify-content-between py-2 px-2 mt-2">
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+/>
+
+<footer class="footer text-light py-3 px-4 mt-4" style="background-color: #2f4050;">
+  <div class="d-flex justify-content-between align-items-center flex-wrap gap-3">
     <div>
-        <strong>Copyright</strong> Example Company &copy; 2014-2018
+      <strong>Codecta</strong> ©️ 2024 - 2025
     </div>
-    <div>
-        10GB of <strong>250GB</strong> Free.
+    <div class="small">
+      +51 922 546 863
     </div>
-</div> -->
-<div class="footer">
-    <div class="float-right">
-        10GB of <strong>250GB</strong> Free.
+    <div class="small">
+      <a href="mailto:codectaperu@gmail.com" class="text-light text-decoration-none">
+        codectaperu@gmail.com
+      </a>
     </div>
-    <div>
-        <strong>Copyright</strong> Example Company &copy; 2014-2018
+
+    <div class="d-flex justify-content-end align-items-center" style="gap: 15px;">
+      <a href="https://wa.me/51922546863" target="_blank" class="text-light">
+        <i class="fab fa-whatsapp fa-lg"></i>
+      </a>
+
+      <a href="https://www.facebook.com/codectaperu/" target="_blank" class="text-light">
+        <i class="fab fa-facebook fa-lg"></i>
+      </a>
+
+      <a href="mailto:codectaperu@gmail.com" class="text-light">
+        <i class="fas fa-envelope fa-lg"></i>
+      </a>
+
+      <a href="https://www.instagram.com/codectaperu/" target="_blank" class="text-light">
+        <i class="fab fa-instagram fa-lg"></i>
+      </a>
     </div>
-</div>
+  </div>
+</footer>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -267,6 +267,7 @@
 
         @include('components.inspinia.footer-inspinia')
     </div>
+    
     </div>
 
 </body>
